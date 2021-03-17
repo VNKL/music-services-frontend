7d3ae34e6562332cbde72275d09b1b9dc0a4b5d8
VNKL <vnkl.iam@gmail.com>
2021-03-17 11:17:49 +0300

    refactor api service

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -60,12 +60,7 @@ export default class ApiService {
     _apiBaseUrl = 'http://127.0.0.1:8000/api/'
     _vkTokenUrl = `https://oauth.vk.com/authorize?client_id=7669131&display=page&redirect_uri=${this._apiBaseUrl}users.bindVk&scope=360448&response_type=code&v=5.126`
 
-    async bindVk(username) {
-        const url = `${this._vkTokenUrl}&state=${username}`
-        window.location.replace(url)
-    }
-
-    async getResponse(method, params) {
+    async _getResponse(method, params) {
         const fullUrl = new URL(`${this._apiBaseUrl}${method}`)
         fullUrl.search = new URLSearchParams(params).toString()
         const response = await fetch(fullUrl, {
@@ -80,66 +75,60 @@ export default class ApiService {
         }
     }
 
+    async bindVk(username) {
+        const url = `${this._vkTokenUrl}&state=${username}`
+        window.location.replace(url)
+    }
+
     async createCampaign(camp_params) {
         const params = this._refactor_camp_params(camp_params)
-        await this.getResponse('ads.createCampaign', params)
+        await this._getResponse('ads.createCampaign', params)
     }
 
     async createAutomate(automate_params) {
         const params = this._refactor_automate_params(automate_params)
-        await this.getResponse('ads.createAutomate', params)
+        await this._getResponse('ads.createAutomate', params)
     }
 
-    async getUser() {
-        const user = await this.getResponse('users.get')
-        if (typeof user !== 'undefined') {
-            return this._unpackUser(user)
-        }
-    }
-
-    async getCampaigns() {
-        const campaigns = await this.getResponse('ads.getAllCampaigns')
-        if (typeof campaigns !== 'undefined') {
-            return this._unpackCampaigns(campaigns)
+    async getAds(campaignId) {
+        const campaign = await this._getResponse('ads.getCampaign', {id: campaignId, extended: 1})
+        if (typeof campaign !== "undefined") {
+            return this._unpackCampaign(campaign)
         }
     }
 
     async getAutomates() {
-        const automates = await this.getResponse('ads.getAllAutomates')
+        const automates = await this._getResponse('ads.getAllAutomates')
         if (typeof automates !== 'undefined') {
             return this._unpackAutomates(automates)
         }
     }
 
-    async getAds(campaignId) {
-        const campaign = await this.getResponse('ads.getCampaign', {id: campaignId, extended: 1})
-        if (typeof campaign !== "undefined") {
-            return this._unpackCampaign(campaign)
-        }
-    }
-
-    async stopAutomate(automateId) {
-        await this.getResponse('ads.stopAutomate', {id: automateId})
-    }
-
-    async updateAdsStat(campaignId) {
-        const campaign = await this.getResponse('ads.updateCampaignStats', {id: campaignId, extended: 1})
-        if (typeof campaign !== "undefined") {
-            return this._unpackCampaign(campaign)
-        }
-    }
-
     async getCabsAndGroups() {
-        const cabsAndGroups = await this.getResponse('ads.getCabinetsAndGroups')
+        const cabsAndGroups = await this._getResponse('ads.getCabinetsAndGroups')
         if (typeof cabsAndGroups !== 'undefined') {
             return this._unpackCabsAndGroups(cabsAndGroups)
         }
     }
 
+    async getCampaigns() {
+        const campaigns = await this._getResponse('ads.getAllCampaigns')
+        if (typeof campaigns !== 'undefined') {
+            return this._unpackCampaigns(campaigns)
+        }
+    }
+
     async getRetarget(cabinet) {
-        const retarget = await this.getResponse('ads.getRetarget', cabinet)
+        const retarget = await this._getResponse('ads.getRetarget', cabinet)
         if (typeof retarget !== 'undefined') {
             return this._unpackRetarget(retarget)
+        }
+    }
+
+    async getUser() {
+        const user = await this._getResponse('users.get')
+        if (typeof user !== 'undefined') {
+            return this._unpackUser(user)
         }
     }
 
@@ -159,45 +148,17 @@ export default class ApiService {
         }
     }
 
-    _unpackUser = (user) => {
-        return {
-            username: user.username,
-            avaUrl: user.ava_url,
-            balance: user.balance,
-            hasToken: user.has_token,
+    async stopAutomate(automateId) {
+        await this._getResponse('ads.stopAutomate', {id: automateId})
+    }
+
+    async updateAdsStat(campaignId) {
+        const campaign = await this._getResponse('ads.updateCampaignStats', {id: campaignId, extended: 1})
+        if (typeof campaign !== "undefined") {
+            return this._unpackCampaign(campaign)
         }
     }
 
-    _unpackCabsAndGroups = (cabsAndGroups) => {
-        return {
-            cabinets: cabsAndGroups.cabinets.map((cabinet) => {
-                return {
-                    cabinetType: cabinet.account_type,
-                    cabinetId: cabinet.account_id,
-                    cabinetName: cabinet.account_name,
-                    clientId: cabinet.client_id,
-                    clientName: cabinet.client_name
-                }
-            }),
-            groups: cabsAndGroups.groups.map((group) => {
-                return {
-                    groupName: group.name,
-                    groupId: group.id,
-                    groupAvaUrl: group.photo_200
-                }
-            })
-        }
-    }
-
-    _unpackRetarget = (retarget) => {
-        return retarget.map((item) => {
-            return {
-                retargetName: item.name,
-                retargetId: item.id,
-                audienceCount: item.audience_count
-            }
-        })
-    }
 
     _refactor_camp_params = (params) => {
         let sex = params.sex
@@ -247,51 +208,6 @@ export default class ApiService {
         return refactored_params
     }
 
-    _unpackCampaigns = (campaigns) => {
-        return campaigns.map((campaign) => {
-            return {
-                campaignId: campaign.campaign_id,
-                status: campaign.status,
-                isAutomate: campaign.is_automate ? 1 : 0,
-                hasModerateAudios: campaign.has_moderate_audios,
-                audiosIsModerated: campaign.audios_is_moderated,
-                artist: campaign.artist,
-                title: campaign.title,
-                name: campaign.campaign_name,
-                spent: campaign.spent.toFixed(2),
-                reach: campaign.reach,
-                cpm: campaign.cpm.toFixed(2),
-                listens: campaign.listens,
-                cpl: campaign.cpl.toFixed(2),
-                ltr: `${(campaign.lr * 100).toFixed(2)}%`,
-                saves: campaign.saves,
-                cps: campaign.cps.toFixed(2),
-                str: `${(campaign.sr * 100).toFixed(2)}%`,
-                cover: campaign.cover_url,
-                date: new Date(campaign.create_date).toLocaleDateString()
-            }
-        })
-    }
-
-    _unpackCampaign = (campaign) => {
-        return {
-            campaignId: campaign.campaign_id,
-            name: `${campaign.artist} - ${campaign.title}`,
-            spent: campaign.spent.toFixed(2),
-            reach: campaign.reach,
-            cpm: campaign.cpm.toFixed(2),
-            listens: campaign.listens,
-            cpl: campaign.cpl.toFixed(2),
-            ltr: `${(campaign.lr * 100).toFixed(2)}%`,
-            saves: campaign.saves,
-            cps: campaign.cps.toFixed(2),
-            str: `${(campaign.sr * 100).toFixed(2)}%`,
-            cover: campaign.cover_url,
-            date: new Date(campaign.create_date).toLocaleDateString(),
-            ads: this._unpackAds(campaign.ads)
-        }
-    }
-
     _unpackAds = (ads) => {
         return ads.map((ad) => {
             return {
@@ -337,4 +253,88 @@ export default class ApiService {
         })
     }
 
+    _unpackCabsAndGroups = (cabsAndGroups) => {
+        return {
+            cabinets: cabsAndGroups.cabinets.map((cabinet) => {
+                return {
+                    cabinetType: cabinet.account_type,
+                    cabinetId: cabinet.account_id,
+                    cabinetName: cabinet.account_name,
+                    clientId: cabinet.client_id,
+                    clientName: cabinet.client_name
+                }
+            }),
+            groups: cabsAndGroups.groups.map((group) => {
+                return {
+                    groupName: group.name,
+                    groupId: group.id,
+                    groupAvaUrl: group.photo_200
+                }
+            })
+        }
+    }
+
+    _unpackCampaign = (campaign) => {
+        return {
+            campaignId: campaign.campaign_id,
+            name: `${campaign.artist} - ${campaign.title}`,
+            spent: campaign.spent.toFixed(2),
+            reach: campaign.reach,
+            cpm: campaign.cpm.toFixed(2),
+            listens: campaign.listens,
+            cpl: campaign.cpl.toFixed(2),
+            ltr: `${(campaign.lr * 100).toFixed(2)}%`,
+            saves: campaign.saves,
+            cps: campaign.cps.toFixed(2),
+            str: `${(campaign.sr * 100).toFixed(2)}%`,
+            cover: campaign.cover_url,
+            date: new Date(campaign.create_date).toLocaleDateString(),
+            ads: this._unpackAds(campaign.ads)
+        }
+    }
+
+    _unpackCampaigns = (campaigns) => {
+        return campaigns.map((campaign) => {
+            return {
+                campaignId: campaign.campaign_id,
+                status: campaign.status,
+                isAutomate: campaign.is_automate ? 1 : 0,
+                hasModerateAudios: campaign.has_moderate_audios,
+                audiosIsModerated: campaign.audios_is_moderated,
+                artist: campaign.artist,
+                title: campaign.title,
+                name: campaign.campaign_name,
+                spent: campaign.spent.toFixed(2),
+                reach: campaign.reach,
+                cpm: campaign.cpm.toFixed(2),
+                listens: campaign.listens,
+                cpl: campaign.cpl.toFixed(2),
+                ltr: `${(campaign.lr * 100).toFixed(2)}%`,
+                saves: campaign.saves,
+                cps: campaign.cps.toFixed(2),
+                str: `${(campaign.sr * 100).toFixed(2)}%`,
+                cover: campaign.cover_url,
+                date: new Date(campaign.create_date).toLocaleDateString()
+            }
+        })
+    }
+
+    _unpackRetarget = (retarget) => {
+        return retarget.map((item) => {
+            return {
+                retargetName: item.name,
+                retargetId: item.id,
+                audienceCount: item.audience_count
+            }
+        })
+    }
+
+    _unpackUser = (user) => {
+        return {
+            username: user.username,
+            avaUrl: user.ava_url,
+            balance: user.balance,
+            hasToken: user.has_token,
+        }
+    }
 }
